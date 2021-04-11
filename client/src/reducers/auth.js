47fd61897a0b1dfec4149bd5119446010783e93e
Dominic Hains <contact.dominichains@gmail.com>
2021-04-11 16:46:46 -0400

    logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,6 +6,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_ERROR,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 const initialState = {
@@ -33,6 +34,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
